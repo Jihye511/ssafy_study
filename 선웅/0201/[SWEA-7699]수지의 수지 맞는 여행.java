40b0286c7464ae52e4f26f736521943a934107f5
TechieMoon <startmoon123@gmail.com>
2025-02-03 00:24:45 +0900

    [SWEA]-7699

--- 선웅/0201/[SWEA-7699]수지의 수지 맞는 여행.java
+++ 선웅/0201/[SWEA-7699]수지의 수지 맞는 여행.java
@@ -1,67 +1,59 @@
+import java.io.*;
 import java.util.*;
-
+ 
 public class Solution {
-    static class Node {
-        String alphabets;
-        int r;
-        int c;
-        int distance;
-
-        public Node(String alphabets, int r, int c, int distance) {
-            this.alphabets = alphabets;
-            this.r = r;
-            this.c = c;
-            this.distance = distance;
-        }
-    }
-
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
-
-        // test case만큼 반복
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int R = sc.nextInt();
-            int C = sc.nextInt();
-            char[][] island = new char[R][C];
+    static int R, C, max;
+    static char[][] board;
+    static boolean[] visited;
+    // 상하좌우 이동을 위한 방향 배열
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+ 
+    public static void main(String[] args) throws Exception {
+        // 입력을 위한 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+         
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+             
+            board = new char[R][C];
             for (int i = 0; i < R; i++) {
-                String line = sc.next();
-                for (int j = 0; j < C; j++) {
-                    island[i][j] = line.charAt(j);
-                }
+                String line = br.readLine();
+                board[i] = line.toCharArray();
             }
-            System.out.println("#" + test_case + " " + bfs(island, R, C));
+             
+            // DFS 탐색 준비
+            max = 0;
+            visited = new boolean[26]; // A~Z 까지 알파벳 방문 체크
+            // 시작 칸 (0,0)의 알파벳 방문 체크
+            visited[board[0][0] - 'A'] = true;
+            dfs(0, 0, 1);
+             
+            // 결과 출력
+            System.out.println("#" + t + " " + max);
         }
-        sc.close();
     }
-
-    public static int bfs(char[][] island, int R, int C) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(island[0][0] + "", 0, 0, 1));
-        int[] dr = { -1, 1, 0, 0 };
-        int[] dc = { 0, 0, -1, 1 };
-        while (true){
-            Node cur = queue.poll();
-            for (int d = 0; d < 4; d++) {
-                int nr = cur.r + dr[d];
-                int nc = cur.c + dc[d];
-
-                if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
-                    boolean stop = false;
-                    for(char c: cur.alphabets.toCharArray()){
-                        if(c == island[nr][nc]){
-                            stop = true;
-                        }
-                    }
-                    if(stop){
-                        continue;
-                    }
-                    queue.offer(new Node(cur.alphabets + island[nr][nc], nr, nc, cur.distance + 1));
-                }
-            }
-            if(queue.isEmpty()){
-                return cur.distance;
+     
+    // (x, y)에서 시작하여 현재까지 count개의 명물을 방문한 상태에서 DFS 수행
+    static void dfs(int x, int y, int count) {
+        max = Math.max(max, count);
+         
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            // 범위 체크
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+             
+            int index = board[nx][ny] - 'A';
+            // 아직 방문하지 않은 명물인 경우
+            if (!visited[index]) {
+                visited[index] = true;
+                dfs(nx, ny, count + 1);
+                // 백트래킹: 다음 경로를 위해 방문 상태를 해제
+                visited[index] = false;
             }
         }
     }
